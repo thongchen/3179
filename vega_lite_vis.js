d2f3d157aa3719beedada8ed7d6aa6968c04a832
Thong Chen Yang <113319906+thongchen@users.noreply.github.com>
2023-10-20 12:27:54 +0800

    Add files via upload
    All files except data folder

---
+++ vega_lite_vis.js
@@ -0,0 +1,19 @@
+var vg_1 = "chart_1.json";
+vegaEmbed("#chart_1", vg_1).then(function(result) {
+
+}).catch(console.error);
+
+var vg_2 = "chart_2.json";
+vegaEmbed("#chart_2", vg_2).then(function(result) {
+
+}).catch(console.error);
+
+var vg_3 = "chart_3.json";
+vegaEmbed("#chart_3", vg_3).then(function(result) {
+
+}).catch(console.error);
+
+var vg_4 = "chart_4.json";
+vegaEmbed("#chart_4", vg_4).then(function(result) {
+
+}).catch(console.error);
